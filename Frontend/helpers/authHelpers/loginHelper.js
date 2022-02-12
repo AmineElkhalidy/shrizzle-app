3deb7be5425e57140a1a17fa3e74685f6b0f8d14
Anas Samoudi <unlimiteddez@gmail.com>
2022-02-12 05:27:46 +0100

    Added helpers for the api

---
+++ Frontend/helpers/authHelpers/loginHelper.js
@@ -0,0 +1,37 @@
+import axios from "axios";
+import { API_URL } from "../../constants/api";
+
+/**
+ *
+ * @param {string} email is the email of the identifier
+ * @param {string} password is the password of the identifier
+ *
+ *
+ * @returns {Object} AuthData with this structure:
+ * {
+ *     @param {string} userId : the user ID
+ *     @param {string} token : the authentication token
+ *     @param {int} tokenExpiration : the number of hours for the token to expire
+ * }
+ */
+
+export const loginHandler = (email, password) => {
+  return axios.post(
+    API_URL,
+    {
+      query: `
+        query{
+            login(email:"${email}",password:"${password}"){
+              userId
+              token
+              tokenExpiration
+            }
+        }`,
+    },
+    {
+      headers: {
+        "Content-Type": "application/json",
+      },
+    }
+  );
+};
--- Frontend/screens/CreateAccount/UserBio.js
+++ Frontend/screens/CreateAccount/UserBio.js
@@ -1,6 +1,4 @@
-import React from 'react';
-
-
+import React from "react";
 import {
   StyleSheet,
   View,
@@ -9,39 +7,42 @@ import {
   Image,
   TextInput,
 } from "react-native";
-import Colors from '../../constants/Colors';
 
-import DefaultButton from "../../components/Buttons/DefaultButton"
-import SkipButton from "../../components/Buttons/SkipButton"
-import DefaultInput from '../../components/InputText/DefaultInput';
+//colors
+import Colors from "../../constants/Colors";
 
+//buttons
+import DefaultButton from "../../components/Buttons/DefaultButton";
+import SkipButton from "../../components/Buttons/SkipButton";
 
 function UserBio() {
-    return <ImageBackground source={require("../../Assets/TestPictures/orange_bg_bottom.png")} style={styles.screen}>
-      
+  return (
+    <ImageBackground
+      source={require("../../Assets/TestPictures/orange_bg_bottom.png")}
+      style={styles.screen}
+    >
       <View style={styles.container}>
         <Text style={styles.title}>Can you tell us a bit about yourself</Text>
 
-        <View  style={styles.inputContainer}>
+        <View style={styles.inputContainer}>
           <View style={styles.inputTextContainer}>
-            <TextInput 
+            <TextInput
               style={styles.input}
-              multiline={true} 
-              numberOfLines={15} 
+              multiline={true}
+              numberOfLines={15}
               placeholder="Please enter your bio..."
             />
           </View>
         </View>
 
-
         <DefaultButton text="Continue" />
-        <SkipButton color="#fff"/>
+        <SkipButton color="#fff" />
       </View>
-    </ImageBackground>;
+    </ImageBackground>
+  );
 }
 
 export default UserBio;
-
 
 const styles = StyleSheet.create({
   screen: {
@@ -50,37 +51,35 @@ const styles = StyleSheet.create({
     position: "relative",
     padding: 0,
   },
-  container:{
+  container: {
     padding: 10,
-    paddingHorizontal:30,
+    paddingHorizontal: 30,
     position: "relative",
-    
   },
-  
+
   title: {
-    paddingTop:30,
-    marginBottom:40,
+    paddingTop: 30,
+    marginBottom: 40,
     color: "#00285c",
     fontSize: 48,
-    fontWeight:"bold",
+    fontWeight: "bold",
   },
-  inputContainer:{
+  inputContainer: {
     width: "100%",
-    marginTop:30
-    
+    marginTop: 30,
   },
   inputTextContainer: {
     width: 350,
     borderColor: Colors.blue,
-    backgroundColor:"white",
+    backgroundColor: "white",
     borderWidth: 1,
     borderRadius: 25,
     marginVertical: 8,
-    paddingVertical:0,
+    paddingVertical: 0,
   },
-  input:{
+  input: {
     width: 250,
     marginHorizontal: 40,
-    textAlign:"left"
-  }
+    textAlign: "left",
+  },
 });
