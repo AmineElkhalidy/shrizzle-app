749f1197c549ebf1432ddf50a1f9f3d3bc0d8f5f
HAJIB Diae <diae.hajib@gmail.com>
2022-02-12 17:03:19 +0100

    Contact list added
    -added contact list screen
    -when rendered, favorite contact are listed first, the rest is ordered as fetched
    -real time search, favorites are listen first during search

---
+++ Frontend/screens/fakeData.js
@@ -0,0 +1,28 @@
+var contacts= [
+    {
+      name:"diae hajib",
+      personal:true,
+      business:true,
+      selected:false,
+      profilepic: null,
+      key:1
+    },
+    {
+      name:"yassir hassani",
+      personal:true,
+      business:false,
+      selected:false,
+      profilepic: null,
+      key:2
+    },
+    {
+      name:"jhon doe",
+      personal:false,
+      business:true,
+      selected:true,
+      profilepic: null,
+      key:3
+    },
+  ];
+  
+  export default contacts;
